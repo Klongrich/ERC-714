f5558ce3aff731d9c4b181efba58f8b7524700c7
Kyle Longrich <longrichk@gmail.com>
2020-11-10 23:58:07 -0500

    adding team address in constructor

--- migrations/2_depoly_contracts.js
+++ migrations/2_depoly_contracts.js
@@ -1,5 +1,5 @@
 const Token = artifacts.require("ERC714");
 
 module.exports = function (deployer) {
-  deployer.deploy(Token);
+  deployer.deploy(Token, "0x9795D52176e5DEa38D2b27B57A0b7480032a0E0c");
 };
