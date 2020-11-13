f3110105a958d1a48ff91ec7a386b72b9ab1521c
Kyle Longrich <longrichk@gmail.com>
2020-11-13 01:42:39 -0500

    very rough draft

--- contracts/IERC714.sol
+++ contracts/IERC714.sol
@@ -24,12 +24,24 @@ interface IERC714 {
      */
     function transfer(address to, uint256 value) external returns (bool);
 
+    /**
+     *  @dev Pays out set amount of ETH to team when the correct time is hit.
+        @param _to address of person purchasing the tokens.
+        @param amount amount of eth that is being sent in exchange for tokens
+        @param _limit The ETH hard cap of the presale
+     */
     function _mint(
         address _to,
         uint256 amount,
         uint256 _limit
     ) external returns (bool);
 
+    /**
+     *  @dev Pays out set amount of ETH to team when the correct time is hit.
+        @param pay_out_time current unix stand time stamp. Checked against 
+        block.timestamp + accurate time within the contract
+        @param team address of the team that recieves payout
+     */
     function pay_out_to_team(uint256 pay_out_time, address payable team)
         external;
 }
