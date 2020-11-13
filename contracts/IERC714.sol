e7a00270cfda100cf0dcb2185a4f36c57f90b257
Kyle Longrich <longrichk@gmail.com>
2020-11-13 01:36:31 -0500

    updating interface

--- contracts/IERC714.sol
+++ contracts/IERC714.sol
@@ -2,35 +2,34 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 interface IERC714 {
-    event Approval(
-        address indexed owner,
-        address indexed spender,
-        uint256 value
-    );
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    function name() external view returns (string memory);
-
-    function symbol() external view returns (string memory);
-
-    function decimals() external view returns (uint8);
-
-    function totalSupply() external view returns (uint256);
-
-    function balanceOf(address owner) external view returns (uint256);
-
-    function allowance(address owner, address spender)
+    /**
+     *  @dev Returns the amount of tokens in existence.
+        @param _limit total amount of ETH allowed raised
+        @param _presalePrice price per token (eth / token) at inital price 
+     */
+    function totalSupply(uint256 _limit, uint256 _presalePrice)
         external
         view
         returns (uint256);
 
-    function approve(address spender, uint256 value) external returns (bool);
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
 
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     * Returns a boolean value indicating whether the operation succeeded.
+     * Emits a {Transfer} event.
+     */
     function transfer(address to, uint256 value) external returns (bool);
 
-    function transferFrom(
-        address from,
-        address to,
-        uint256 value
+    function _mint(
+        address _to,
+        uint256 amount,
+        uint256 _limit
     ) external returns (bool);
+
+    function pay_out_to_team(uint256 pay_out_time, address payable team)
+        external;
 }
